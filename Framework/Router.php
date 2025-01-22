e0958f7fcc9e2d0750617abb3f3da6b80ea9ee8a
RGenovas <r.genovas@gmail.com>
2025-01-22 15:06:31 +0200

    refactoring

--- Framework/Router.php
+++ Framework/Router.php
@@ -12,6 +12,8 @@
 
 namespace Framework;
 
+use App\Controllers\ErrorController;
+
 class Router
 {
     protected $routes = [];
@@ -22,15 +24,19 @@ class Router
      *
      * @param string $method
      * @param string] $uri
-     * @param string $controller
+     * @param string $action
      * @return void
      */
-    public function registerRoute($method, $uri, $controller)
+    public function registerRoute($method, $uri, $action)
     {
+        list($controller, $controllerMethod) = explode('@', $action);
+
+
         $this->routes[] = [
             'method' =>  $method,
             'uri' => $uri,
-            'controller' => $controller
+            'controller' => $controller,
+            'controllerMethod' => $controllerMethod
         ];
     }
 
@@ -88,17 +94,6 @@ class Router
         $this->registerRoute('DELETE', $uri, $controller);
     }
 
-    /**
-     * @param int $httpCode
-     * @return void
-     * 
-     */
-    public function error($httpCode = 404)
-    {
-        http_response_code($httpCode);
-        loadView("error/{$httpCode}");
-        exit;
-    }
 
 
 
@@ -115,10 +110,17 @@ class Router
     {
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === $method) {
-                require basePath('App/' . $route['controller']);
+                // Extract controller and controller method
+                $controller = 'App\\Controllers\\' . $route['controller'];
+                $controllerMethod = $route['controllerMethod'];
+
+                // Instantiate the controller and call the method
+
+                $controllerInstance = new $controller();
+                $controllerInstance->$controllerMethod();
                 return;
             }
         }
-        $this->error();
+        ErrorController::notFound();
     }
 }
--- routes.php
+++ routes.php
@@ -9,8 +9,12 @@
 //     // '404' => '/controllers/error/404.php'
 // ];
 
+$router->get('/workopia1/public/', 'HomeController@index');
+$router->get('/workopia1/public/listings', 'ListingController@index');
+$router->get('/workopia1/public/create', 'ListingController@create');
+$router->get('/workopia1/public/listing', 'ListingController@show');
 
-$router->get('/workopia1/public/', '/controllers/home.php');
-$router->get('/workopia1/public/listings', '/controllers/listings/index.php');
-$router->get('/workopia1/public/create', '/controllers/listings/create.php');
-$router->get('/workopia1/public/listing', '/controllers/listings/show.php');
+// $router->get('/workopia1/public/', '/controllers/home.php');
+// $router->get('/workopia1/public/listings', '/controllers/listings/index.php');
+// $router->get('/workopia1/public/create', '/controllers/listings/create.php');
+// $router->get('/workopia1/public/listing', '/controllers/listings/show.php');
